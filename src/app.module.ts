db21a538a2835dfaf454d4a09f048963f91ae97a
Edgar Espinetti <edgarespinetti@macbookpro.fibertel.com.ar>
2025-09-19 23:43:58 -0300

    add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import {TcpListener} from "./infrastructure/adapters/TcpListener";
 import {MessageProcessor} from "./application/services/MessageProcessor";
 import {InMemoryMessageRepository} from "./infrastructure/repositories/InMemoryMessageRepository";
+import {AppController} from "./app.controller";
 
 @Module({
+  controllers: [AppController],
   providers: [
     TcpListener,
     MessageProcessor,
